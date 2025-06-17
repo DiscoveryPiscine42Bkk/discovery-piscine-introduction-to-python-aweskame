fc2c556c714315fe63de209a38979929f2366fd5
aweskame <68030317@kmitl.ac.th>
2025-06-17 01:58:27 +0000

    อัปเดตงาน4

--- cell01/ex00/name.py
+++
@@ -1,3 +0,0 @@
-first_name = "Arissara"
-last_name = "Weskamee"
-print(first_name,last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Arissara"
+last_name = "Weskamee"
+whole_name = first_name + " " + last_name
+print(whole_name)
